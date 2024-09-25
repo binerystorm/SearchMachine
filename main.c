fbd244d17637bbe45c0fe8dcb83b9fcfb032306f
binerystorm <gerick.de.kruyf@gmail.com>
2024-09-25 08:05:30 +0200

    added token frequency and stripped redundent info
    (semi functional) I gave each token a frequency and I parsed punctuation and html tags out
    of files being parsed. I downlouded the pygame docs to test this code so
    it will not work in another enviroment without the pygame docs.

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 // TODO(gerick): create my own assertion function
 #include <assert.h>
 
+#define ArrayLen(ARR) sizeof((ARR))/sizeof((ARR)[0])
+
 // TODO(gerick): 
 // NOTE(gerick):
 //TODO(gerick): make this function "better"
@@ -66,59 +68,102 @@ bool str_eq(const Str lft, const Str rgt)
     return true;
 }
 
-#define ArrayLen(ARR) sizeof((ARR))/sizeof((ARR)[0])
-int main(){
-    const char *file =  //"  hello  there hello do  duude\n there";
-        slurp_file_or_panic("main.c");
-    Str roam = {
-        file,
-        cstr_len(file)
-    };
-    // TODO(gerick): consider making this a hash table
-    Str tokens[sizeof(Str)*1024*3] = {};
+static inline bool is_space(char c)
+{  
+    return c == ' ' ||
+           c == '\n' ||
+           c == '\t';
+}
 
-    while(roam.len > 0){
-    // stripping white space
+static inline bool is_punctuation(char c)
+{
+    // TODO(gerick): make more robust punctuation filter
+    return (c >= 33 && c <= 47) || c == '?' || c == ';';
+}
+
+static inline void str_shift_left(Str *str){
+    str->data++;
+    str->len--;
+}
+
+void parse_file(Str roam_buffer, Str *token_map, size_t *freq_map, size_t map_len){
+    // TODO(gerick): create more rigorouse tokenizer, rather than one that only works on white space
+    while(roam_buffer.len > 0){
+        // stripping redundent information e.g white space, punctuation, html tags
         for(;
-            roam.len > 0 &&
-            (*roam.data == ' ' ||
-            *roam.data == '\n' ||
-            *roam.data == '\t');
-            // TODO(gerick): extract to function
-            roam.data++, roam.len--);
+            (roam_buffer.len > 0) &&
+            (is_space(*roam_buffer.data) ||
+            is_punctuation(*roam_buffer.data) ||
+            *roam_buffer.data == '<');
+            str_shift_left(&roam_buffer))
+        {
+            // TODO(gerick): research whether this way of filtering out html tags will lose information
+            if(*roam_buffer.data == '<'){
+                for(;(roam_buffer.len > 0) &&
+                     (*(roam_buffer.data) != '>');
+                    str_shift_left(&roam_buffer));
+            }
+        }
 
         // pulling token out of stream
-        // TODO(gerick): create more rigorouse tokenizer, rather than one that only works on white space
-        const char * const token_start_loc = roam.data;
+        const char * const token_start_loc = roam_buffer.data;
         for(;
-            roam.len > 0 &&
-            *roam.data != ' ' &&
-            *roam.data != '\n' &&
-            *roam.data != '\t';
-            roam.data++, roam.len--);
+            (roam_buffer.len > 0) &&
+            (!is_space(*roam_buffer.data) && 
+            !is_punctuation(*roam_buffer.data) &&
+            *roam_buffer.data != '<');
+            str_shift_left(&roam_buffer));
+        
+        // TODO(gerick): this skips tokens the end in `<` e.g. `foo<` will be skipped
+        if(*roam_buffer.data == '<'){continue;}
 
+        // TODO(gerick): copy tokens into own memory buffer, rather than borrowing them from the roam_buffer
         const Str token = {
             token_start_loc,
-            (size_t)roam.data - (size_t)token_start_loc
+            (size_t)roam_buffer.data - (size_t)token_start_loc
         };
 
         // putting token in token set
-        for(size_t cur = 0; 
-        cur < ArrayLen(tokens);
-        cur++)
+        for(size_t cur = 0;;cur++)
         {
-            if(tokens[cur].data == 0){
-                tokens[cur].data = token.data;
-                tokens[cur].len = token.len;
+            if(cur >= map_len){return;}
+            if(token_map[cur].data == 0){
+                token_map[cur].data = token.data;
+                token_map[cur].len = token.len;
+                freq_map[cur] = 1;
                 break;
             }
-            if(str_eq(tokens[cur], token)){
+            if(str_eq(token_map[cur], token)){
+                freq_map[cur] += 1;
                 break;
             }
         }
     }
-    for(size_t cur = 0; cur < ArrayLen(tokens) && tokens[cur].data != 0; cur++){
-        printf("(%.*s)\n", (int)tokens[cur].len, tokens[cur].data);
+}
+
+int main(){
+    const char *files[] = {
+        "./pygame-docs/ref/bufferproxy.html",
+        "./pygame-docs/ref/camera.html",
+        "./pygame-docs/ref/cdrom.html",
+    };
+    // TODO(gerick): consider making this a hash table
+
+    for(size_t i = 0; i < ArrayLen(files); i++){
+        Str tokens[sizeof(Str)*1024*3] = {};
+        size_t freq[sizeof(size_t)*1024*3] = {};
+        char *file = slurp_file_or_panic(files[i]);
+         Str roam_buffer = {
+             file,
+             cstr_len(file)
+         };
+        parse_file(roam_buffer, tokens, freq, ArrayLen(tokens));
+        for(size_t cur = 0; cur < ArrayLen(tokens) && tokens[cur].data != 0; cur++){
+            if(tokens[cur].len >= 20){continue;}
+            printf("(%.*s):(%zu)\n", (int)tokens[cur].len, tokens[cur].data, freq[cur]);
+        }
+        printf("\n");
+        free(file);
     }
     return 0;
 }
